3b403442cbe46d06a6cd19bb119c28f8217413c3
madhurimach12 <82215587+madhurimach12@users.noreply.github.com>
2021-05-01 14:26:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 let screen = document.getElementById('screen');
 buttons = document.querySelectorAll('button');
 let screenValue = '';
+
 for (item of buttons) {
     item.addEventListener('click', (e) => {
         buttonText = e.target.innerText;
         console.log('Button text is ', buttonText);
+        if (buttonText == ' ')
+        {
+          screenValue += buttonText;
+ 			screen.value = screenValue;  
+        }
         if (buttonText == 'X') {
             buttonText = '*';
             screenValue += buttonText;
